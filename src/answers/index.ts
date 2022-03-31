c1426adcc045bd78bafabbaefa05d391f7ee269e
Alex Chen <alexchen45@gmail.com>
2022-03-31 11:27:48 +0800

    a

--- src/answers/index.ts
+++ src/answers/index.ts
@@ -6,7 +6,7 @@ import json from 'https://alexchen45.github.io/luckidleAns.json' assert {type:"j
 
 export function getAnswerOfDay(day: number) {
 
-  const [word = '', hint = ''] =  ['冰鲜妙鹅', '鹅']
+  const [word = '', hint = ''] =  ['福里福气', '福']
 
   return {
     word,
