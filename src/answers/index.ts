45f20799be6e66c601a630f49108cd6034cc17a9
Alex Chen <alexchen45@gmail.com>
2022-04-02 20:13:03 +0800

    cm

--- src/answers/index.ts
+++ src/answers/index.ts
@@ -6,7 +6,7 @@ import json from 'https://alexchen45.github.io/luckidleAns.json' assert {type:"j
 
 export function getAnswerOfDay(day: number) {
 
-  const [word = '', hint = ''] =  ['小福词语', '小']
+  const [word = '', hint = ''] =  ['鲨枝招展', '鲨']
 
   return {
     word,
